1fdaf8f2996f0021f66bbca97636a341e79f899c
levtsun-yevheniia <levtsun.yevheniia@gmail.com>
2023-04-17 01:28:27 +0300

    First version

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function Cart() {
+  return (
+    <div className="main_title">
+      <div className="container__indent"></div>
+      <h1>Cart</h1>
+      <div class="cart">
+        <h1>Сart</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
